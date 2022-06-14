cb872cb6c4761945cc74d5cc26317f52e2b990f2
AndrewVis <97456285+AndrewVis@users.noreply.github.com>
2022-06-14 11:25:53 -0400

    minor changes

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ renderer.render(scene, camera);
 // Torus
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const material = new THREE.MeshStandardMaterial({ color: 0xaf47ff });
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
